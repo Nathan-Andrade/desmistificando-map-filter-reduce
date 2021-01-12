b6c6d9b1fae1f2978c537171920a0697ef438cc5
Nathan Andrade <nathandulino2003@gmail.com>
2021-01-12 20:59:43 -0200

    filter

---
+++ filter.js
@@ -0,0 +1,19 @@
+const pets = [
+  {
+    name: 'rex',
+    type: 'Dog',
+    age: 5
+  },
+  {
+    name: 'Miau',
+    type: 'cat',
+    age: 5
+  },
+  {
+    name: 'glup',
+    type: 'fish',
+    age: 5
+  }
+]
+
+console.log(pets)
