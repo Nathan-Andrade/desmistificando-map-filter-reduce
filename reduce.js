cd0d9b46ec51b6162cac29f96e666c11ec3e58a8
Nathan Andrade <nathandulino2003@gmail.com>
2021-01-12 22:05:09 -0200

    array.reduce()

--- reduce.js
+++ reduce.js
@@ -37,10 +37,42 @@ const pets = [
   }
 ]
 
-const totalWeight = pets.reduce((total, pet) => {
-  console.log(`Total: ${total}, Pet: ${pet.name}`)
-
-  return total + pet.weight
-}, 0)
+// calculando o peso total de todos os animais
+//const totalWeight = pets.reduce((total, pet) => {
+//  return total + pet.weight
+//}, 0)
 
 console.log(totalWeight)
+
+
+//calculando a idade e o peso de todos os animais
+
+//const totalWeight = pets.reduce((total, pet) => {
+//  return {
+//  totalAge: total.totalAge + pet.age,
+//  totalWeight: total.totalWeight + pet.weight
+//}
+//}, {totalAge: 0, totalWeight: 0})
+//console.log(totalWeight)
+
+
+// verificando os cachorros
+
+//const totalWeight = pets.reduce((total, pet) => {
+// if (pet.type !== 'dog') return total
+//
+//  return total + pet.weight
+//}, 0)
+//console.log(totalWeight)
+
+
+//encadeando tudo
+const totalWeightDogs = pets
+  .filter((pet) => {
+    return pet.type === 'dog'
+  })
+  .reduce((total, pet) => {
+    return total + pet.weight
+  }, 0)
+
+console.log(totalWeightDogs)
